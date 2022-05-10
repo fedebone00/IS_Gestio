253db9f255e9eba7b84350f0da08483ec5e07e19
cancianilorenzo <lorenzo.canciani@studenti.unitn.it>
2022-05-10 21:05:01 +0200

    First implementation test

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+
 export default function Home() {
   return (
-    <div className='sidebar'>
-      
+    <div className={styles.sidebar}>
+      <h1 className={styles.sidebarTitle}>Gestione Azienda</h1>
+
+      <Link href="/" className={styles.sidebarElement1}><a>Pagina Iniziale</a></Link>
+
     </div>
   )
 }
