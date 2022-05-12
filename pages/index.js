f395419f8a8d2424270429f85b56833dc3936fdf
bottarocarlo <bottarocarloo@gmail.com>
2022-05-12 16:23:02 +0200

    side bar adjustment

--- pages/index.js
+++ pages/index.js
@@ -7,44 +7,35 @@ import home from '../public/home-outline.png'
 
 export default function Home() {
   return (
-    <div className={styles.sidebar}>
-      <ul>
-        <li>
-          <div><h1 className={styles.sidebarTitle}>Area dipendente</h1></div>
-        </li>
-        <li >
-          <a href="login">
-            <h3 className={styles.sidebarElement1}>Pagina iniziale</h3>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            <h3 className={styles.sidebarElement1}>Timbra presenze</h3>
-          </a>
-        </li>
-        <li>
-
-          <a href="/">
-            <h3 className={styles.sidebarElement1}>Gestione ferie</h3>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            <h3 className={styles.sidebarElement1}>Notifica malattia</h3>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            <h3 className={styles.sidebarElement1}>Contatta colleghi</h3>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            <h3 className={styles.sidebarElement1}>Cloud aziendale</h3>
-          </a>
-        </li>
-    </ul>
-
+    <div className="flex flex-col fixed left-0 top-0 w-64 h-screen bg-gray-700 text-center gap-6">
+      <div></div>
+      <h1 className=" font-extrabold text-2xl text-gray-400 py-4">Area dipendente</h1>
+      <a href="login">
+        <h3 className= " text-gray-400 ">Pagina iniziale</h3>
+      </a>
+      <a href="/">
+        <h3 className="   text-gray-400 ">Timbra presenze</h3>
+      </a>
+      <a href="/"> 
+        <h3 className="   text-gray-400 ">Gestione ferie</h3>
+      </a>
+      <a href="/">
+        <h3 className="   text-gray-400 ">Notifica malattia</h3>
+      </a>
+      <a href="/">
+        <h3 className="   text-gray-400 ">Contatta colleghi</h3>
+      </a>
+      <a href="/">
+        <h3 className="   text-gray-400 ">Cloud aziendale</h3>
+      </a>
+      <div>
+      <Image
+      src="/public/logo.png"
+      alt="logo"
+      width={500}
+      height={500}
+      /> 
+      </div>
     </div>
   )
 }
