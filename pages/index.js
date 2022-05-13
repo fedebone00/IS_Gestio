973a327b567655de099b34037d15409e31c2f66b
bottarocarlo <bottarocarloo@gmail.com>
2022-05-13 16:43:41 +0200

    sidebar icon

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import home from '../public/home-outline.png'
-import { IoHomeOutline } from 'react-icons/io';
+import React, { Component } from 'react';
+import {MdFirstPage
+,  MdOutlineLogin
+,  MdBeachAccess
+,  MdLocalHospital
+,  MdContacts
+,  MdCloud} from 'react-icons/md';
+
+
 
 
 export default function Home() {
@@ -12,22 +20,23 @@ export default function Home() {
       <div></div>
       <h1 className=" font-extrabold text-2xl text-gray-400 py-4">Area dipendente</h1>
       <a href="login">
-        <h3 className= " text-gray-400 "><IoHomeOutline/>Pagina iniziale</h3>
+        <h3 className= " text-gray-400 inline-flex items-center justify-center "><MdFirstPage fontSize={20}/> Pagina iniziale</h3>
       </a>
+      
       <a href="/">
-        <h3 className="   text-gray-400 ">Timbra presenze</h3>
+        <h3 className="   text-gray-400 inline-flex items-center justify-center"><MdOutlineLogin fontSize={20}/>Timbra presenze</h3>
       </a>
       <a href="/"> 
-        <h3 className="   text-gray-400 ">Gestione ferie</h3>
+        <h3 className="   text-gray-400 inline-flex items-center justify-center"><MdBeachAccess fontSize={20}/>Gestione ferie</h3>
       </a>
       <a href="/">
-        <h3 className="   text-gray-400 ">Notifica malattia</h3>
+        <h3 className="   text-gray-400 inline-flex items-center justify-center"><MdLocalHospital fontSize={20}/>Notifica malattia</h3>
       </a>
       <a href="/">
-        <h3 className="   text-gray-400 ">Contatta colleghi</h3>
+        <h3 className="   text-gray-400 inline-flex items-center justify-center"><MdContacts fontSize={20}/>Contatta colleghi</h3>
       </a>
       <a href="/">
-        <h3 className="  text-gray-400 ">Cloud aziendale</h3>
+        <h3 className="  text-gray-400 inline-flex items-center justify-center"><MdCloud fontSize={20}/>Cloud aziendale</h3>
       </a>
       <div className='py-96'>
       <Image
