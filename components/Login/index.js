2ca74e068be7d1df38a8570bbd98889502df888c
bottarocarlo <bottarocarloo@gmail.com>
2022-05-15 12:06:33 +0200

    complete the login page and API

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
+import Image from 'next/image'
 import axios from 'axios'
+
 
 export function LoginForm() {
   const [username, setUsername] = useState("");
@@ -74,10 +76,22 @@ axios
   }
 
   return (
+    <div>
+      <Image
+      layout="fill"
+      className="object-center object-cover pointer-events-none"
+      src="/../public/bg.png"
+      alt="title"
+    />
     <form onSubmit={handleSubmit}>
-      <fieldset>
-        <legend>GESTIO</legend>
-        <div>
+      <fieldset className="  relative z-1  p-3 flex flex-col space-y-3 justify-center items-center  h-screen" >
+      <Image
+      src="/../public/logo.png"
+      alt="GESTIO"
+      width={155}
+      height={130}
+      /> 
+        <div className=" p-2 border  rounded flex flex-row  justify-center items-center">
           <input
             type="text"
             id="usernameInput"
@@ -85,7 +99,7 @@ axios
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div>
+        <div className="border p-2 rounded flex flex-row  justify-center items-center ">
           <input
             type="password"
             id="passwordInput"
@@ -98,10 +112,11 @@ axios
             {errorMessage}
           </div>
         )}
-        <button type="submit" disabled={isLoading}>
+        <button type="submit" disabled={isLoading} className="py-2 inline-block px-6 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-gray-400 hover:shadow-lg focus:bg-gray-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">
           LOGIN
         </button>
       </fieldset>
     </form>
+    </div>
   );
 }
