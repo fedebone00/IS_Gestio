5282c9ce19f2342ae331cf56b24f84f782767a0a
cancianilorenzo <lorenzo.canciani@studenti.unitn.it>
2022-05-16 21:21:12 +0200

    Added sassy-datepicker , need to fix position of object

--- pages/areaDipendente.js
+++ pages/areaDipendente.js
@@ -4,12 +4,28 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import home from '../public/home-outline.png'
 import React, { Component } from 'react';
-import {MdFirstPage, MdOutlineLogin , MdBeachAccess , MdLocalHospital , MdContacts , MdCloud} from 'react-icons/md';
+import { MdFirstPage, MdOutlineLogin, MdBeachAccess, MdLocalHospital, MdContacts, MdCloud } from 'react-icons/md';
 import { SidebarDip } from "../components/sidebarDip";
+import DatePicker from 'sassy-datepicker'; //Calendar component
 
 
-export default function Home() {
+
+export default function HomeDip() {
+  
+  const onChange = (date) => {
+    console.log(date.toString());
+  }
+  
+
   return (
-    <SidebarDip/>
-  )
+    <div className="flex flex-nowrap">
+    <div><SidebarDip /></div>
+    <div className="absolute top-30 right-20">
+      <DatePicker onChange={onChange} />
+    </div>
+    <div className="absolute top-[3px] left-[55px]">
+      <h1>NON FUNZIONA!!!!!!!</h1>
+    </div>
+  </div>
+  );
 }
