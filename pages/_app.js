b1c4980bd7d0a6e22ab3eb81b84761f7f3be79f9
bottarocarlo <bottarocarloo@gmail.com>
2022-05-17 16:32:47 +0200

    favico.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+  <link rel="shortcut icon" href="/favicon.ico" />
+  </Head>
+
   return <Component {...pageProps} />
 }
 
