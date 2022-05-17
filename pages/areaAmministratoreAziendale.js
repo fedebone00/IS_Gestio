b68936f0a86583ef2ae98f8e54b6cbba01375ec5
bottarocarlo <bottarocarloo@gmail.com>
2022-05-17 15:34:24 +0200

    component adjustment

--- pages/areaAmministratoreAziendale.js
+++ pages/areaAmministratoreAziendale.js
@@ -4,15 +4,17 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import home from '../public/home-outline.png'
 import React, { Component } from 'react';
-import { MdFirstPage, MdPersonAddAlt1, MdPersonRemoveAlt1, MdOutlineModeEditOutline, MdOutlineEditRoad, MdTextFields } from 'react-icons/md';
-import { SidebarAA } from "../components/sidebarAA";
+import {MdFirstPage, MdOutlineLogin , MdBeachAccess , MdLocalHospital , MdContacts , MdCloud} from 'react-icons/md';
+import { SidebarDip } from '../components/sidebarDip'
+import { SidebarAA } from '../components/sidebarAA'
+import TopBar from '../components/topBar'
 
 
 export default function Home() {
   return (
     <div>
-    <SidebarAA/>
-    <TopBar/>
-  </div>
+      <SidebarAA/>
+      <TopBar/>
+    </div>
   )
 }
