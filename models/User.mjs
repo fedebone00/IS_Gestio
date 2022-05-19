592ae37394b312820b1288882108569e103885ed
fedebone00 <fedebone00@hotmail.it>
2022-05-19 08:37:52 +0200

    updated User model

--- models/User.mjs
+++ models/User.mjs
@@ -1,5 +1,12 @@
 import { mongoose } from 'mongoose';
 
-const User = mongoose.model("user", {email: String, password_hash: String, salt: String, role: String});
+const userSchema = new mongoose.Schema({
+    email: String,
+    password_hash: String,
+    salt: String,
+    role: String
+});
+
+const User = mongoose.model("user", userSchema);
 
 export default User;
