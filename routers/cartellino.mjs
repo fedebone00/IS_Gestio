1a0a1c0ce9c97504c7caff3166595b2a1a723c2c
Gio200023 <lunardigiovanni@icloud.com>
2022-05-23 10:34:40 +0200

    added Cartellino models and router for check-in and check-out

---
+++ routers/cartellino.mjs
@@ -0,0 +1,39 @@
+const app = require('../app/app.mjs')
+const Cartellino = require('../models/Cartellino.mjs')
+const {isAuthenticated, isAuthorized} = require('../auth_middleware/auth.mjs')
+
+app.get('/timbratura', isAuthenticated, isAuthorized, (req,res) =>{
+    Cartellino.find().then((cartellino) => res.send(cartellino))
+});
+
+app.post('/timbratura', isAuthenticated, isAuthorized, (req,res) => {
+    let errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    
+    let c = new Cartellino({data: req.body['data'],tipo: req.body['tipo'], ora: req.body['ora'], smartworking: req.body['smartworking']});
+    c.save()
+        .then(() => res.status(201).send(`Succesfully checked ${req.body.tipo}`))
+        .catch(() => res.status(500).send(`Error checking ${req.body.tipo}`));
+});
+
+app.delete('/timbratura/:id', isAuthenticated, isAuthorized , (req,res) => {
+
+    Cartellino.findByIdAndRemove(req.params.id)
+        .then(() => res.status(201).send(`Succesfully removed: ${req.params.id}`))
+        .catch(() => res.status(500).send(`Error deleting: ${req.params.id}`));
+});
+
+app.patch('/timbratura/:id', isAuthenticated, isAuthorized, async (req, res) => {
+
+    Cartellino.findByIdAndUpdate({
+        _id:req.params.id
+    },{
+        $set:req.body
+    }).then(()=> {
+        res.status(201).json({message:"success"});
+    }).catch(err =>{
+        res.status(500).send(err.message);
+    });
+});
