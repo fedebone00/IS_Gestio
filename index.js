8a1d4d932776a78bf03bf5fa388668a348d19b00
Gio200023 <lunardigiovanni@icloud.com>
2022-05-23 11:04:39 +0200

    added and changed Malattia modles and routers ways of import and exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const app = require('./app/app.js');
+const malattia = require('./routers/malattia.js')
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to database"))
