a9635a05a1062a9aaab1797b9ce5fa461a1cd2c3
cancianilorenzo <lorenzo.canciani@studenti.unitn.it>
2022-05-31 22:23:04 +0200

    Fixed Bacheca API

--- routers/bacheca.js
+++ routers/bacheca.js
@@ -7,13 +7,13 @@ app.get('/api/v1/bacheca', isAuthenticated, isAuthorized, (req,res) =>{
     Bacheca.find().then((bacheca) => res.send(bacheca))
 });
 
-app.post('/api/v1/bacheca', isAuthenticated, isAuthorized,check('testoAnnuncio').notEmpty(), check('dataAnnuncio').notEmpty(), (req,res) => {
+app.post('/api/v1/bacheca', isAuthenticated, isAuthorized,check('testoAnnuncio').notEmpty(), check('scadenzaAnnuncio').notEmpty(), (req,res) => {
     let errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
     
-    let c = new Bacheca({data: req.body['testoAnnuncio'],tipo: req.body['dataAnnuncio']});
+    let c = new Bacheca({testoAnnuncio: req.body['testoAnnuncio'], scadenzaAnnuncio: req.body['scadenzaAnnuncio']});
     c.save()
         .then(() => res.status(201).send(`Succesfully insert announcement ${req.body.tipo}`))
         .catch(() => res.status(500).send(`Error creation announcement ${req.body.tipo}`));
@@ -21,14 +21,14 @@ app.post('/api/v1/bacheca', isAuthenticated, isAuthorized,check('testoAnnuncio')
 
 app.delete('/api/v1/bacheca/:id', isAuthenticated, isAuthorized , (req,res) => {
 
-    Cartellino.findByIdAndRemove(req.params.id)
+    Bacheca.findByIdAndRemove(req.params.id)
         .then(() => res.status(201).send(`Succesfully removed: ${req.params.id}`))
         .catch(() => res.status(500).send(`Error deleting: ${req.params.id}`));
 });
 
 app.patch('/api/v1/bacheca/:id', isAuthenticated, isAuthorized, async (req, res) => {
 
-    Cartellino.findByIdAndUpdate({
+    Bacheca.findByIdAndUpdate({
         _id:req.params.id
     },{
         $set:req.body
