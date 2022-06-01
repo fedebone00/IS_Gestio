5d36564d395c77a440cf8c1eb6e2a7f696f44bc7
cancianilorenzo <lorenzo.canciani@studenti.unitn.it>
2022-06-01 22:01:10 +0200

    Fixed page rimuoviDipendente, works also with API

--- pages/rimuoviDipendente.js
+++ pages/rimuoviDipendente.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { SidebarAA } from "../components/sidebarAA";
 import { TopBar } from "../components/topBar";
 
-
 function parseJwt(token) {
   if (!token) {
     return;
@@ -17,59 +16,77 @@ export default function rimuoviDipendente() {
   const [jwt, setJwt] = useState("");
   const [rt, setRt] = useState("");
   const [set, setSet] = useState(1);
-  const [base, setBase] = useState(
-    //"https://gestio-is.herokuapp.com/api/v1/dipendente"
-    "http://localhost:8080/api/v1/dipendente"
-  );
   const [slash, setSlash] = useState("/");
-  const [id, setId] = useState("");
   const [error, setError] = useState(false);
   const [token, setToken] = useState("");
-  const [variabileProva, setVariabileProva] = useState("")
+  const [variabileProva, setVariabileProva] = useState("");
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+  const [data, setData] = useState("");
+  const [livello, setLivello] = useState("");
+  const [id, setId] = useState("");
+  const [rimuovi, setRimuovi] = useState("");
 
-  
-
-  async function handleRemove(e) {
+  async function handleGet(e) {
     e.preventDefault();
+    try {
+      const axios = require("axios");
 
+      let response = await axios({
+        method: "GET",
+        url: "http://localhost:8080/api/v2/dipendentespecifico",
+        headers: {
+          "x-access-token": jwt,
+          email: email,
+        },
+      }).then(function (response) {
+        let risposta = response.data;
+        console.log("RESPONSE-->", risposta);
+        setNome(risposta.nome);
+        setCognome(risposta.cognome);
+        setLivello(risposta.livello);
+        setData(risposta.data);
+        setId(risposta._id);
+        setRimuovi("Rimuovi il dipendente selezionato");
+      });
+    } catch (error) {
+      setRimuovi("Errore nella ricerca!!");
+    } finally {
+    }
+  }
 
-    
-
-
-
+  async function handleRimuovi(e) {
+    e.preventDefault();
+    let url = "http://localhost:8080/api/v1/dipendente/" + id;
     try {
       const axios = require("axios");
 
       let response = await axios({
         method: "DELETE",
-        url: base,
+        url: url,
         headers: {
           "x-access-token": jwt,
         },
-        data: {
-          email,
-        },
       }).then(function (response) {
-        let token = response.data;
-        console.log("RESPONSE-->", token);
+        let risposta = response.data;
+        console.log("RESPONSE-->", risposta);
+        setRimuovi("Rimosso con successo!");
       });
     } catch (error) {
-      if (error.response) {
-      } else if (error.request) {
-      } else {
-      }
+      setRimuovi("Errore nella rimozione!!");
     } finally {
-      //setVariabileProva("Rimosso con successo")
+      
     }
   }
 
   useEffect(() => {
     setTimeout(() => {
-      if(set == 1){
-      setJwt(localStorage.getItem("jwt"));
-      setRt(localStorage.getItem("rt"));
-      console.log("JWT-->", jwt);
-      setSet(0);}
+      if (set == 1) {
+        setJwt(localStorage.getItem("jwt"));
+        setRt(localStorage.getItem("rt"));
+        console.log("JWT-->", jwt);
+        setSet(0);
+      }
     }, 50);
   }, []);
 
@@ -90,7 +107,7 @@ export default function rimuoviDipendente() {
         <div>
           <SidebarAA />
           <TopBar />
-          <form onSubmit={handleRemove}>
+          <form onSubmit={handleGet}>
             <div className="  flex flex-col gap-5 justify-center items-center">
               <input
                 className="border  rounded p-2"
@@ -99,40 +116,32 @@ export default function rimuoviDipendente() {
                 placeholder="Username Dipendente"
                 onChange={(e) => setUsername(e.target.value)}
               />
-              
+
               <button
                 type="submit"
-                className="relative  py-2 inline-block px-6 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
+                className="relative  py-2 inline-block px-6 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-yellow-700 hover:shadow-lg focus:bg-yellow-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"
               >
-                Rimuovi
+                Cerca
               </button>
-              <h1 dangerouslySetInnerHTML={{ __html: variabileProva }} /> 
+
+              <h1>Nome</h1>
+              <div className="border  rounded p-2">{nome}</div>
+              <h1>Cognome</h1>
+              <div className="border  rounded p-2">{cognome}</div>
+              <h1>Livello</h1>
+              <div className="border  rounded p-2">{livello}</div>
+              <h1>Email</h1>
+              <div className="border  rounded p-2">{email}</div>
+              <button
+                className="relative  py-2 inline-block px-6 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
+                onClick={handleRimuovi}
+              >
+                {rimuovi}
+              </button>
             </div>
           </form>
         </div>
       );
-    } else if (parseJwt(jwt).role == "AA" && error != false) {
-      <div>
-        <SidebarAA />
-        <TopBar />
-        <form onSubmit={handleRemove}>
-          <div className="  flex flex-col gap-5 justify-center items-center">
-            <input
-              className="border  rounded p-2"
-              type="text"
-              id="usernameInput"
-              placeholder="Username Dipendente"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="relative  py-2 inline-block px-6 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Rimuovi
-            </button>
-          </div>
-        </form>
-      </div>;
     } else {
       return (
         <div>
