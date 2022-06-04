59048e033f547606454938ffd868e91a488983a8
Gio200023 <lunardigiovanni@icloud.com>
2022-06-04 13:41:07 +0200

    adjust css style

--- components/Login/index.js
+++ components/Login/index.js
@@ -110,7 +110,7 @@ export function LoginForm() {
             <button
               type="submit"
               disabled={isLoading}
-              className="flex py-2 inline-block px-28 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-gray-400 hover:shadow-lg focus:bg-gray-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out"
+              className="flex py-2  px-28 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md  hover:bg-gray-400 hover:shadow-lg focus:bg-gray-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out"
             >
               LOGIN
             </button>
