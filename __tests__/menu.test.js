e8ba2f1299e4692e423142af472f447ae56efafd
Gio200023 <lunardigiovanni@icloud.com>
2022-06-05 22:02:51 +0200

    added menus get to test_api_menu

--- __tests__/menu.test.js
+++ __tests__/menu.test.js
@@ -3,14 +3,19 @@ const request = require('supertest');
 const jwt = require('jsonwebtoken');
 const mongoose = require("mongoose");
 require("dotenv").config();
+const menu = require("../models/Menu.js")
 
 jest.setTimeout(5000);
 
+let dbBACKUPUSER;
 beforeAll( async () => {
     jest.setTimeout(8000);
     app.locals.db = await mongoose.connect(process.env.MONGO_URL);
+    dbBACKUPUSER= await menu.find({}).exec()
 })
-afterAll( () =>{
+afterAll( async ()  =>{
+    await menu.deleteMany({});
+    await menu.insertMany(dbBACKUPUSER)
     mongoose.connection.close(true);
 })
 
@@ -47,14 +52,23 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
         .expect(201)
     });
 
-    //TODO --> non riesco a mandarlo, timeout
+    test('<201> GET display all Menu', () => {
+        return request(app).get('/api/v1/menu/all')
+        .set('x-access-token', token).set('Accept', 'application/json')
+        .expect(201)
+    });
 
-    // test('<404> GET Menu from a not existing data', () => {
-    //     return request(app).get('/api/v1/menu')
-    //     .set('data','20/05/1920')
-    //     .set('x-access-token', token).set('Accept', 'application/json')
-    //     .expect(404)
-    // });
+    test('<201> GET display specific Menu based on id', () => {
+        return request(app).get('/api/v1/specifico/6281f3ae0919d180b35b7e5e/')
+        .set('x-access-token', token).set('Accept', 'application/json')
+        .expect(201)
+    });  
+
+    test('<404> GET display specific Menu based on id', () => {
+        return request(app).get('/api/v1/specifico/6281f3ae0919d180b35b7e23/')
+        .set('x-access-token', token).set('Accept', 'application/json')
+        .expect(404)
+    });  
 
     let id="1gr34gewfsd534231";
 
@@ -64,16 +78,9 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
         .expect(500)
     });    
 
-    test('<400> DELETE forget to parse id menu', () => {
-        return request(app).delete(`/api/v1/menu/627e09fa9141a0d16860d204/`)
-        .send({id:""})
-        .set('x-access-token', token).set('Accept', 'application/json')
-        .expect(400)
-    });    
-
     test('<201> DELETE delete existing Menu', () => {
-        return request(app).delete(`/api/v1/menu/627e09fa9141a0d16860d204/`)
-        .send({id:"627e09fa9141a0d16860d204"})
+        return request(app).delete(`/api/v1/menu/6281f3ae0919d180b35b7e5e/`)
+        .send({id:"6281f3ae0919d180b35b7e5e"})
         .set('x-access-token', token).set('Accept', 'application/json')
         .expect(201)
     }); 
@@ -86,7 +93,7 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
     }); 
 
     test('<201> PATCH modify existing Menu', () => {
-        return request(app).delete(`/api/v1/menu/627e09fa9141a0d16860d204/`)
+        return request(app).delete(`/api/v1/menu/6281f3ae0919d180b35b7e5e/`)
         .send({data:"20/05/2022",secondo:"pesce"})
         .set('x-access-token', token).set('Accept', 'application/json')
         .expect(201)
