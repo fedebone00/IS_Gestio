7b400d6838b61c59a1154b39fa7a563f5a4da8c3
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2022-06-05 11:09:30 +0200

    Update modificaInformazioniDipendente.js

--- pages/modificaInformazioniDipendente.js
+++ pages/modificaInformazioniDipendente.js
@@ -1,6 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import home from '../public/home-outline.png'
 import React, { Component, useEffect, useState } from 'react';
 import { SidebarAA } from "../components/sidebarAA";
 import { TopBar } from "../components/topBar";
+import cookieCutter from 'cookie-cutter'
+import { MdEmail } from 'react-icons/md'
+
 
 function parseJwt(token) {
   if (!token) {
