66a8a007b17c024ff96190e769a7bd7e6ca389b6
bottarocarlo <bottarocarloo@gmail.com>
2022-06-06 18:14:00 +0200

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,6 @@ export default function Login() {
   return (
     <div>
       <LoginForm />
-      <br></br>
-      <br></br>
-      <h1>Da togliere perchè gestito on the fly</h1>
-      <a href="areaAmministratoreAziendale">
-        <h3 className="   text-gray-400 inline-flex items-center justify-center">Area Amministratore Aziendale</h3>
-      </a>
-      <a href="areaDipendente">
-        <h3 className="   text-gray-400 inline-flex items-center justify-center">Area Dipendente</h3>
-      </a>
     </div> 
   );
 }
