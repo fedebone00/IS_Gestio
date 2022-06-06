c81f63c3c729f528513128a969c823ff74877313
Gio200023 <lunardigiovanni@icloud.com>
2022-06-06 15:14:02 +0200

    fixed some api test

--- __tests__/menu.test.js
+++ __tests__/menu.test.js
@@ -2,38 +2,41 @@ const app = require('../app/app');
 const request = require('supertest');
 const jwt = require('jsonwebtoken');
 const mongoose = require("mongoose");
-require("dotenv").config();
-const menu = require("../models/Menu.js")
+// require("dotenv").config();
+const Menu = require('../models/Menu.js');
 
 jest.setTimeout(5000);
 
-let dbBACKUPUSER;
+var menus, userDIP0;
 beforeAll( async () => {
     jest.setTimeout(8000);
     app.locals.db = await mongoose.connect(process.env.MONGO_URL);
-    dbBACKUPUSER= await menu.find({}).exec()
+
+    menus = new Menu({primo:"pasta",secondo:"carne",dolce:"tiramisu",data:'1111/06/07'})
+    await menus.save();
+
 })
 afterAll( async ()  =>{
-    await menu.deleteMany({});
-    await menu.insertMany(dbBACKUPUSER)
+
     mongoose.connection.close(true);
 })
 
-describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
+describe('Menu TESTS', () => {
 
-    var token = jwt.sign({email:"carlo@carletto.it"}, process.env.JWT_SIGN_KEY, {expiresIn: 86400});
-    header={'Content-Type':'application/json', token:token};
+    let token = jwt.sign({email:"carlo@carletto.it",password:"password"},process.env.JWT_SIGN_KEY, {expiresIn: 89000})
+    
+    it('<201> POST create new Menu', async () => {
+        console.log(menus.primo)
+        var res = await request(app).post('/api/v1/menu')
+            .set('x-access-token', token).set('Accept', 'application/json')
+            .send({primo:menus.primo,secondo:menus.secondo, dolce:menus.dolce,data:"1234/12/34"});
 
-    test('<201> POST create new Menu with right body', () => {
-        return request(app).post('/api/v1/menu')
-        .send({primo:"pasta", secondo:"carne", dolce:"tiramisu",data:"20/05/2022"})
-        .set('x-access-token', token).set('Accept', 'application/json')
-        .expect(201)
-    });
+        expect(res.status).toBe(201);
+    })
 
     test('<401> POST create new Menu with already existed date', () => {
         return request(app).post('/api/v1/menu')
-        .send({primo:"pasta", secondo:"carne", dolce:"tiramisu",data:"20/05/2022"})
+        .send({primo:"pasta", secondo:"carne", dolce:"tiramisu",data:"1234/12/34"})
         .set('x-access-token', token).set('Accept', 'application/json')
         .expect(401)
     });
@@ -45,11 +48,11 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
         .expect(400)
     });       
 
-    test('<201> GET Menu from a specified data', () => {
-        return request(app).get('/api/v1/menu')
-        .set({data:"20/05/2022"})
-        .set('x-access-token', token).set('Accept', 'application/json')
-        .expect(201)
+    it('<201> GET Menu from a specified data', async() => {
+        var res = await request(app).get('/api/v1/menu/all')
+            .set('x-access-token', token).set('Accept', 'application/json')
+
+        expect(res.status).toBe(201)
     });
 
     test('<201> GET display all Menu', () => {
@@ -61,7 +64,7 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
     // TODO --> dice che id non esiste
 
     // test('<201> GET display specific Menu based on id', () => {
-    //     return request(app).get('/api/v1/specifico/6281f3ae0919d180b35b7e5e/')
+    //     return request(app).get('/api/v1/specifico/6281f9837c0e28530a26604a')
     //     .set('x-access-token', token).set('Accept', 'application/json')
     //     .expect(201)
     // });  
@@ -71,8 +74,6 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
         .set('x-access-token', token).set('Accept', 'application/json')
         .expect(404)
     });  
-
-    let id="1gr34gewfsd534231";
 
     test('<500> DELETE not existing menu', () => {
         return request(app).delete(`/api/v1/menu/1gr34gewfsd534231`)
@@ -87,26 +88,19 @@ describe('[SUPERTEST] [LOGGATI]  /api/v1/menu', () => {
         .expect(201)
     }); 
 
-    // test('<500> PATCH modify not existing Menu', () => {
-    //     return request(app).patch(`/api/v1/menu/6281f905ec73c4832a922365`)
-    //     .send({primo:"risotto"})
-    //     .set('x-access-token', token).set('Accept', 'application/json')
-    //     .expect(500)
-    // }); 
-
-    // test('<200> PATCH modify existing Menu', () => {
-    //     return request(app).patch(`/api/v1/menu/6281fa03ee67f2d4a379f9ae/`)
-    //     .send({data:"20/05/123",secondo:"pesce"})
-    //     .set('x-access-token', token).set('Accept', 'application/json')
-    //     .expect(200)
-    // }); 
-
-    test('<404> PATCH modify existing Menu', () => {
-        return request(app).patch(`/api/v1/menu/6281f905ec73c4832a922323/`)
-        .send({data:"20/05/12",secondo:"pesce"})
+    test('<404> PATCH modify not existing Menu', () => {
+        return request(app).patch(`/api/v1/menu/627e09d79141a0d16860d123`)
+        .send({primo:"risotto"})
         .set('x-access-token', token).set('Accept', 'application/json')
         .expect(404)
     }); 
+
+    // test('<200> PATCH modify existing Menu', () => {
+    //     return request(app).patch(`/api/v1/menu/6281f8bd7a155cae5036c951/`)
+    //     .send({secondo:"pesce"})
+    //     .set('x-access-token', token).set('Accept', 'application/json')
+    //     .expect(200)
+    // }); 
 
 })
 
